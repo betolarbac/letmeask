36682c1a5e12b0c61340e27e5f4eba6e17234bcf
betolarbac <betocr.dev@gmail.com>
2021-11-26 00:30:19 -0300

    pasta components add

---
+++ src/components/button.tsx
@@ -0,0 +1,14 @@
+//capsula de botão 
+
+import { ButtonHTMLAttributes } from "react"
+
+import '../styles/button.scss';
+
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+    return (
+        <button className="button" {...props}/>
+    )
+}
