978ed6f2ae16b295e7807553e81e6a8168894d41
betolarbac <betocr.dev@gmail.com>
2021-11-26 21:57:04 -0300

    melhorado fluxo do app e mudando logica para outras pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { createContext, useState } from 'react'
-
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from "./pages/home";
-
 import { NewRoom } from "./pages/NewRoom";
 
+import {AuthContextProvider } from '../src/contexts/AuthContext'
 
-export const TestContext = createContext({} as any);
+
+
 
 function App() {
-  const [value, setValue] = useState('teste')
 
   return (
     //forma de criar rotas de forma nova 
     <BrowserRouter>
-    <TestContext.Provider value={{value, setValue}}>
+    <AuthContextProvider>
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<NewRoom />} path="/rooms/new" />
       </Routes>
-      </TestContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
